1d515a221ac2bd02cc8470a0b47414037fc49777
Muhamad Alfian Widjaya <822134@hq.bni.co.id>
2023-08-02 17:23:24 +0700

    done

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/SpaceTourism",  
+  base: "https://alfianw.github.io/Space-Tourism",  
   plugins: [react()],
 
 })
