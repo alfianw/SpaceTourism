4280015403adc2a0b8b918344cfe66bc705b4520
Muhamad Alfian Widjaya <822134@hq.bni.co.id>
2023-08-02 15:09:31 +0700

    done

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,9 @@
-// vite.config.js 
-import { defineConfig } from  'vite' 
-import react from  '@vitejs/plugin-react' 
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/ 
-export  default  defineConfig ( ( { command } ) => { 
-  const config = { 
-    plugins : [ react ()], 
-    base : '/' , 
-  } 
-
-  if (command !== 'serve' ) { 
-    config.base = '/Space-Tourism/'   } return config })
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: "/Space-Tourism",
+  plugins: [react()],
+  
+})
