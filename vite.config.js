6dd0407206557c57b46e4dcf7d76ff9f7a2fb4e1
Muhamad Alfian Widjaya <822134@hq.bni.co.id>
2023-08-02 15:03:49 +0700

    coba

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js 
+import { defineConfig } from  'vite' 
+import react from  '@vitejs/plugin-react' 
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+// https://vitejs.dev/config/ 
+export  default  defineConfig ( ( { command } ) => { 
+  const config = { 
+    plugins : [ react ()], 
+    base : '/' , 
+  } 
+
+  if (command !== 'serve' ) { 
+    config.base = '/Space-Tourism/'   } return config })
